4d25aa2201e0a780539b3f8cb88ba9b31b5a35ac
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-02-13 17:22:18 +0100

    [lessonOne-exercise] solution to lessonOne part one exercise

---
+++ lessonOne/exercise/exercise-01.dart
@@ -0,0 +1,37 @@
+// Assignment 1
+// Create an int variable crush and initialize it with 5. Print it out.
+// Create a double variable wallet and set it to 2025.5. Print it out.
+// Create a String variable name "Valentine". Print it out.
+// Create a bool variable isSingle and set it to true. Print it out.
+//
+// Finally print out this statement making use of the declared variable
+//
+// name has crush ladies he is crushing on but has only wallet amount to gift them on name day 
+//
+//
+// Where I indicate bold, call the variables that you have declared there
+
+void main() {
+  // crush variable and initialize with 5;
+  int crush = 5;
+  // log crush;
+  print("crush: $crush");
+
+  //initialize variable;
+  double wallet = 2025.5;
+  // log wallet;
+  print("wallet: $wallet");
+
+  // initialize variable;
+  String name = "Valentine";
+  // log name;
+  print("name: $name");
+
+  //initialize variable;
+  bool isSingle = true;
+  // log isSingle;
+  print("is he single $isSingle");
+
+  // log sentence
+  print("$name has $crush ladies he is crushing on but has only N$wallet amount to gift them on $name day");
+}
